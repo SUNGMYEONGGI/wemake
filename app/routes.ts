057ce83dced00c9b846806c80669b672b64e20ca
MyeonggiSeong <myeonggi.seong@day1company.co.kr>
2025-04-05 18:12:11 +0900

    Update project files

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/about", "potato/tomato.tsx"),
+  //route("/about", "potato/tomato.tsx"),
 ] satisfies RouteConfig;
